6a122b65ee0027942c2cfa8703848a3d8a2aaec9
Diana <taaldiana@gmail.com>
2023-01-10 11:41:57 +0200

    expense date props added js+css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,26 @@
 //1. komponent. See teema paneb äpi tööle
-function ExpenseItem() {
+
+import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props){
+    const month = props.expenseData.date.toLocaleString('en-US', {month: 'long'})
+    const day = props.expenseData.date.toLocaleString('en-US', {day: '2-digit'})
+    const year = props.expenseData.date.getFullYear()
+
     return (
-        <div>
-            <div>Date</div>
+        <div className="expense-item">
             <div>
-                <h2>Title</h2>
-                <div>Amount</div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}€</div>
             </div>
         </div>
     )
 }
-
 export default ExpenseItem
