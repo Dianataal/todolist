3b516bf25df54713929fd290924daaceaecc4635
Diana <taaldiana@gmail.com>
2023-01-25 20:58:07 +0200

    ikka ei tööta. peaks olema 1. ülesande etapp aga ma ei saa õigeks.. liigun edasi

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,20 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
-import Card from "../UI/Card";
 
 function ExpenseItem(props){
-    const ExpenseItem = (props) => {
-        const clickHandler = () => {
-            console.log('Clicked!')
-        }
 
-        return (
+    return (
+        <div className="expense-item">
             <Card className="expense-item">
-                @@ -11,8 +14,10 @@ function ExpenseItem(props){
-                <h2>{props.expenseData.title}</h2>
-                <div className="expense-item__price">{props.expenseData.price}€</div>
+                <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+                <div className="expense-item__description">
+                    <h2>{props.expenseData.title}</h2>
+                    <div className="expense-item__price">{props.expenseData.price}€</div>
                 </div>
-                <button onClick={clickHandler}>Change Title</button>
-                </Card>
-                )
+        </div>
+</Card>
+)
+}
 
-            }
-
-                export default ExpenseItem
+export default ExpenseItem
