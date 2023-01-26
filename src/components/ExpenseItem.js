4944db90687ab64098d88ee98959b912f4b0dc36
Diana <taaldiana@gmail.com>
2023-01-26 13:21:08 +0200

    UI/Card lisatud aga ei tööta. Expenses kaust lisatud

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,23 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+const clickHandler = () => {
+    console.log('Clicked!')
+}
+
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
-                <div className="expense-item__price">{props.expenseData.price}€</div>
+                <div className="expense-item__price">{props.expenseData.price}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
     )
+
 }
 
 export default ExpenseItem
