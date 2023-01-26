be465fbb15e93db90c0444c7f5a927e3e072282e
Diana <taaldiana@gmail.com>
2023-01-26 12:28:54 +0200

    ül 1 jaoks õige!

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
 
-function ExpenseItem(props){
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <Card className="expense-item">
-                <ExpenseDate date={props.expenseData.date}></ExpenseDate>
-                <div className="expense-item__description">
-                    <h2>{props.expenseData.title}</h2>
-                    <div className="expense-item__price">{props.expenseData.price}€</div>
-                </div>
-            </Card>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}€</div>
+            </div>
         </div>
-)
+    )
 }
 
 export default ExpenseItem
